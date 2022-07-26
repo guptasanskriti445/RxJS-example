4a25b832af7ca6a7d0e26ed25a9d3288b6bd5509
guptasanskriti445 <guptasanskriti445@gmail.com>
2022-07-26 09:05:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-amjs78
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-amjs78)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  AsyncStream(observer) {
+    var t1 = setInterval(() => {
+      observer.next(Math.random() * 10);
+    }, 1000);
+  }
+  myobserv = Observable.create(this.AsyncStream);
+  mainstreamobservable = Observable.create(this.AsyncStream);
+  step1stream = this.mainstreamobservable.pipe(
+    map((x: number) => Math.round(x))
+  );
+  step2stream = this.step1stream.pipe(filter((x: number) => x > 4));
+  ngOnInit() {
+    // this.myobserv.subscribe((res) => this.Listener(res));
+    this.step2stream.subscribe((res) => this.Listener(res));
+  }
+  Listener(res: any) {
+    console.log(res);
+  }
+}
